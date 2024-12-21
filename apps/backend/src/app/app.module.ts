f2b2eddb5ea6c8bac0a0f38862c97ca3eb13d3de
PappasFreetas <nick.pappas5424@gmail.com>
2024-12-20 21:53:50 -0500

    updated docker compose to include postgres, and added User entity

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT, 10) || 5432,
+      username: process.env.DB_USER || 'postgres',
+      password: process.env.DB_PASSWORD || 'password',
+      database: process.env.DB_NAME || 'messenger',
+      autoLoadEntities: true, // auto loads entities from project
+      synchronize: true, // auto synchs the db schema (disable in prod)
+    }),
+    TypeOrmModule.forFeature([User]), // Register User entity
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
